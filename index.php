6a2bde9b7d9d4b3baeb63795077e478a78784642
unexdesigns <unexdesigns@gmail.com>
2017-02-21 19:21:33 +0200

    header

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+	<!--Meta -->
+	<title>Amberagė - Gintaro dirbiniai</title>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700&amp;subset=latin-ext,vietnamese" rel="stylesheet">-->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,700|Source+Serif+Pro:400,600,700&amp;subset=latin-ext,vietnamese" rel="stylesheet">
+
+
+
+
+	<!-- Favicons -->
+	
+		<link rel="apple-touch-icon" sizes="180x180" href="/static/img/favicon/apple-touch-icon.png">
+		<link rel="icon" type="image/png" href="/static/img/favicon/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="/static/img/favicon/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="/static/img/favicon/manifest.json">
+		<meta name="apple-mobile-web-app-title" content="Amberagė">
+		<meta name="application-name" content="Amberagė">
+		<meta name="theme-color" content="#ffc40d">
+
+	<link rel="stylesheet" href="/static/css/style.css" />
+</head>
+<body>
+	<header>
+		<div class="topbar">
+			<div class="container">
+				<div class="topbar-right">
+					<div class="topbar-item">
+						<a href="#">Mano paskyra</a>
+					</div>
+					<div class="topbar-item">
+						<a href="#">Pirkti</a>
+					</div>
+					<div class="topbar-item">
+						<a href="#">Krepšelis <span class="cart-counter">0</span></a>
+					</div>
+				</div>
+				<div class="topbar-item">
+					<i class="material-icons">phone</i> +370 601 11110
+				</div>
+				<div class="topbar-item">
+					<i class="material-icons">mail</i> <a href="mailto:info@amberage.com">info@amberage.com</a>
+				</div>
+			</div>
+		</div>
+		<div class="branding">
+			<div class="brand-image"><a href="/"><img src="/static/img/logo_hor.png" alt="Amberagė logotipas" /></a></div>
+		</div>
+		<nav>
+			<ul class="nav">
+				<li><a href="#">Pradžia</a></li>
+				<li class="dropdown-container">
+					<a class="dropdown-toggle" href="#">Gintaro gaminiai</a>
+					<ul class="dropdown-list dropdown-double">
+						<li class="top-section">
+							<div class="form-group mb25">
+								<input type="text" class="form-control-custom" placeholder="Ieškoti" />
+							</div>
+						</li>
+						<li class="double-list">
+							<ul>
+								<li class="subheading"><a href="#">Papuošalai</a></li>
+								<li><a href="#">Apyrankės</a></li>
+								<li><a href="#">Auskarai</a></li>
+								<li><a href="#">Karoliai</a></li>
+								<li><a href="#">Koljė</a></li>
+								<li><a href="#">Pakabukai</a></li>
+								<li><a href="#">Sagės</a></li>
+								<li><a href="#">Komplektai</a></li>
+								<li><a href="#">Žiedai</a></li>
+								<li><a href="#">Vaikiškos apyrankės</a></li>
+								<li><a href="#">Vaikiški karoliai</a></li>
+								<li><a href="#">Vaikiški komplektai</a></li>
+								<li><a href="#">Vyriški karoliai</a></li>
+								<li><a href="#">Papuošalai gyvūnams</a></li> 
+							</ul>
+							<ul>
+								<li class="subheading"><a href="#">Suvenyrai</a></li>
+								<li><a href="#">Paveikslai iš gintaro gabaliukų</a></li>
+								<li><a href="#">Paveikslai su gintaru</a></li>
+								<li><a href="#">Suvenyrai su gintaru</a></li>
+								<li><a href="#">Gintaro dirbiniai</a></li>
+								<li><a href="#">Raktų pakabukai</a></li>
+								<li><a href="#">Magnetai</a></li>
+							</ul>
+						</li>  
+						<li class="clearfix"> 
+							<a href="#" class="btn-plain pull-right"><b>Rodyti viską</b></a>
+						</li>
+					</ul>
+				</li>
+				<li><a href="#">Apie mus</a></li>
+				<li><a href="#">Naujienos</a></li>
+				<li><a href="#">Populiariausi gaminiai</a></li>
+				<li><a href="#">Nuolaidos</a></li>
+			</ul>
+		</nav>
+	</header>   
+	<main>
+		<section class="catalog">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						
+					</div>
+					<div class="col-md-8">
+						
+					</div>
+				</div>
+			</div>	
+		</section>
+	</main>
+	<script type="text/javascript" src="/static/js/compiler-dist.js"></script>
+</body>
+</html>
